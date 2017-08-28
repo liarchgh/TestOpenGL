dc6cbd7fe4f4fb75cb9d19f17d9018eb7a7ff7a0
liarchgit <liarchwy@163.com>
2017-08-28 23:26:44 +0800

    try triangle and snow

--- TestOpenGL/Square.cpp
+++ TestOpenGL/Square.cpp
@@ -1,6 +1,14 @@
 #include <GL/glut.h>
 #include <cstdio>
 
+void init() {
+	glClearColor(0.0, 0.0, 0.0, 0.5);
+	glColor3f(1.0, 1.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+}
+
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_POLYGON);
@@ -12,8 +20,13 @@ void display() {
 	glFlush();
 }
 
-int main() {
+int main(int argc, char ** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(1600, 900);
+	glutInitWindowPosition(100, 100);
 	glutCreateWindow("square");
 	glutDisplayFunc(display);
+	init();
 	glutMainLoop();
 }
