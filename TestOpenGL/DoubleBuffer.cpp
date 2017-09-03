7afe60904e16532e5e04215ba5876d2c28004c79
liarchgit <liarchwy@163.com>
2017-09-03 22:31:46 +0800

    uinte them

--- TestOpenGL/DoubleBuffer.cpp
+++ TestOpenGL/DoubleBuffer.cpp
@@ -5,42 +5,47 @@
 #include <vector>
 using namespace std;
 
-const int DESIRED_FPS = 30, mxv = 30;
-int mxNumCircles = 20, mxRadius = 40, mnRadius = 3,
-winWid = 800, winHei = 600;
+const int DESIRED_FPS = 120, mxv = 10;
+int mxNumCircles = 1000, mxRadius = 40, mnRadius = 3,
+winWid = 800, winHei = 600,
+choose;
 struct circle {
 	int x, y, radius;
 	unsigned char color;
 	struct {
 		int x, y;
 	}v;
-	circle(int a, int b, int c, int d, int e, unsigned char f) {
-		x = a, y = b, radius = c;
-		v.x = d, v.y = e;
-		color = f;
-	}
-	void move(int val) {
-		x += v.x * val;
-		y += v.y * val;
-		if (x >= winWid) {
-			x = winWid;
-			v.x = -v.x;
-		}
-		else if (x <= 0) {
-			x = 0;
-			v.x = -v.x;
-		}
-		if (y >= winHei) {
-			y = winHei;
-			v.y = -v.y;
-		}
-		else if(y <= 0) {
-			y = 0;
-			v.y = -v.y;
-		}
-	}
+    circle(int a, int b, int c, int d, int e, unsigned char f);
+    void move(int val);
 };
 vector<circle>circles;
+
+void myInit();
+void myKey(unsigned char key, int x, int y);
+void myMouse(int btn, int state, int x, int y);
+void addCircle(int x, int y);
+void myDisplay();
+void drawCircles();
+void myReshape(int w, int h);
+void updateTime(int val);
+
+void drawSquare();
+void drawSquares();
+
+void drawCube();
+
+void initDrawSquareCircle();
+void setOpenGL();
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("TestOpenGL");
+
+    setOpenGL();
+	return 0;
+}
 
 void myReshape(int w, int h) {
 	winWid = w;
@@ -53,13 +58,13 @@ void myReshape(int w, int h) {
 	glLoadIdentity();
 }
 
-void updateCircles(int val) {
+void updateTime(int val) {
 	for (int i = 0; i < circles.size(); ++i) {
 		circle& now = circles[i];
 		now.move(val);
 	}
 	glutPostRedisplay();
-	glutTimerFunc((1000.0) / DESIRED_FPS, updateCircles, 1);
+	glutTimerFunc((1000.0) / DESIRED_FPS, updateTime, 1);
 }
 
 void drawCircles() {
@@ -67,20 +72,12 @@ void drawCircles() {
 		int radius = circles[i].radius, x = circles[i].x,
 			y = circles[i].y;
 		glColor3ub((char)rand() % 255, (char)rand() % 255, (char)rand() % 255);
-		glBegin(GL_POLYGON);
-		glVertex2f(0.0, 0.0);
-		glVertex2f(0.0, 5.0);
-		glVertex2f(5.0, 5.0);
-		glEnd();
 		glBegin(GL_TRIANGLE_FAN);
 		for (double j = 0.0, stepRa = 0.1; j < 3.1 * 2; j += stepRa) {
-			int tx = x + radius * cos(j),
+			float tx = x + radius * cos(j),
 				ty = y +  radius * sin(j);
 			glVertex2f(tx, ty);
-			//printf("--------%f %f %f\n", j, radius * cos(j), radius * sin(j));
-			//printf("---%d %d\n", tx - x, ty - y);
 		}
-		//printf("--\n");
 		glEnd();
 	}
 }
@@ -88,18 +85,24 @@ void drawCircles() {
 void myDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    drawSquares();
 	drawCircles();
 	glutSwapBuffers();
 }
 
+void addCircle(int x, int y){
+    if (circles.size() < mxNumCircles) {
+        int radius = rand() % mxRadius, vx = rand() % mxv - mxv / 2, vy = rand() % mxv - mxv / 2;
+        circles.push_back(circle(x, y, radius, vx, vy, (char)rand() % 255));
+        printf("There are %d circles.\n", circles.size());
+        glutPostRedisplay();
+    }
+}
+
 void myMouse(int btn, int state, int x, int y) {
 	//while clicking left button of mouse
-	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		if (circles.size() < mxNumCircles) {
-			int radius = rand() % mxRadius, vx = rand() % mxv - mxv/2, vy = rand() % mxv - mxv/2;
-			circles.push_back(circle(x, y, radius, vx, vy,(char)rand() % 255));
-			glutPostRedisplay();
-		}
+	if (choose == 0 && btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        addCircle(x, y);
 	}
 	//for (int i = 0; i < circles.size(); ++i) {
 	//	printf("--%d %d %d\n", circles[i].x, circles[i].y, circles[i].radius);
@@ -110,27 +113,95 @@ void myKey(unsigned char key, int x, int y) {
 	if (key == 'q' || key == 'Q') {
 		exit(0);
 	}
+    if (choose == 0 && key == 'C' || key == 'c') {
+        circles.clear();
+    }
 }
 
 void myInit() {
 	glClearColor(0.0, 0.5, 0.0, 1.0);
-	glShadeModel(GL_FLAT);
+	//glShadeModel(GL_FLAT);
 	srand((unsigned)time(NULL));
 }
 
-int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(800, 600);
-	glutCreateWindow("Create circles");
-	
+circle::circle(int a, int b, int c, int d, int e, unsigned char f) {
+    x = a, y = b, radius = c;
+    v.x = d, v.y = e;
+    color = f;
+}
+void circle::move(int val) {
+    x += v.x * val;
+    y += v.y * val;
+    if (x >= winWid) {
+        x = winWid;
+        v.x = -v.x;
+    }
+    else if (x <= 0) {
+        x = 0;
+        v.x = -v.x;
+    }
+    if (y >= winHei) {
+        y = winHei;
+        v.y = -v.y;
+    }
+    else if(y <= 0) {
+        y = 0;
+        v.y = -v.y;
+    }
+}
+
+void drawSquare() {
+    glColor3f(1.0, 0.0, 0.0);
+    double a = 20, ox = 50, oy = 50;
+        //ox = winWid / 2 + 50, oy = winHei / 2 + 50;
+    //printf("%lf\n", a);
+    glBegin(GL_POLYGON);
+        glVertex3d(ox+a, oy+a, 0.0);
+        glVertex3d(ox+a, oy-a, 0.0);
+        glVertex3d(ox-a, oy-a, 0.0);
+        glVertex3d(ox-a, oy+a, 0.0);
+    glEnd();
+    //glBegin(GL_TRIANGLE_FAN);
+    //    glVertex2f(a, a);
+    //    glVertex2f(a, -a);
+    //    glVertex2f(-a, -a);
+    //    glVertex2f(-a, a);
+    //glEnd();
+}
+
+void drawSquares() {
+    double  ox = winWid/2, oy = winHei/2;
+    for (int i = 0; i < 8; ++i) {
+        glPushMatrix();
+        glTranslated(ox, oy, 0.0);
+        glRotated(i * 45.0, 0, 0, 1.0);
+        //glScaled(0.5, 0.5, 0.5);
+        drawSquare();
+        glPopMatrix();
+    }
+}
+
+void setOpenGL() {
+    printf("Choose:\n");
+    printf("0: draw squares and circles\n");
+    scanf_s("%d", &choose);
+    switch (choose) {
+        case 0:
+            initDrawSquareCircle();
+            break;
+        default:
+            printf("Nothin.");
+            break;
+    }
+}
+
+void initDrawSquareCircle() {
 	glutInitDisplayMode(GL_DOUBLE | GL_RGB);
 	glutDisplayFunc(myDisplay);
 	glutMouseFunc(myMouse);
 	glutKeyboardFunc(myKey);
 	glutReshapeFunc(myReshape);
-	glutTimerFunc((1000.0) / DESIRED_FPS, updateCircles, 1);
-
+	glutTimerFunc((1000.0) / DESIRED_FPS, updateTime, 1);
+    glutMotionFunc(addCircle);
 	glutMainLoop();
-	return 0;
 }
--- TestOpenGL/SnowFractal.cpp
+++ TestOpenGL/SnowFractal.cpp
@@ -1,88 +1,88 @@
-#include <GL/freeglut.h>
-#include <iostream>
-#include <cstdio>
-using namespace std;
-
-GLfloat v[3][2] = { -0.5, -0.29,
-	0.5, -0.29,
-	0.0, 0.575};
-int n;
-
-void CalDS(GLfloat* a, GLfloat* b, GLfloat* c, GLfloat* v0, GLfloat* v1, GLfloat* v2) {
-	for (int i = 0; i < 2; ++i) {
-		v0[i] = (b[i] - a[i]) / 3 + a[i];
-		v2[i] = (a[i] - b[i]) / 3 + b[i];
-		GLfloat mid = (a[i] + b[i]) / 2;
-		GLfloat t = mid + (mid - c[i]) / 3;
-		v1[i] = t;
-	}
-}
-
-void DrawTriangle(GLfloat* a, GLfloat* b, GLfloat* c) {
-	glVertex2fv(a);
-	glVertex2fv(b);
-	glVertex2fv(c);
-	//getchar();
-}
-
-void DivideSnow(GLfloat* a, GLfloat* b, GLfloat* c, int m) {
-	if (m > 0) {
-		GLfloat v0[2], v1[2], v2[2];
-		GLfloat t0[2], t1[2], t2[2], t3[2], t4[2], t5[2];
-		CalDS(a, b, c, v0, v1, v2);
-		for (int i = 0; i < 2; ++i) {
-			t0[i] = v0[i];
-			t1[i] = v2[i];
-		}
-		DivideSnow(v0, v1, v2, m - 1);
-		CalDS(b, c, a, v0, v1, v2);
-		for (int i = 0; i < 2; ++i) {
-			t2[i] = v0[i];
-			t3[i] = v2[i];
-		}
-		DivideSnow(v0, v1, v2, m - 1);
-		CalDS(c, a, b, v0, v1, v2);
-		for (int i = 0; i < 2; ++i) {
-			t4[i] = v0[i];
-			t5[i] = v2[i];
-		}
-		DivideSnow(v0, v1, v2, m - 1);
-		DivideSnow(a, t0, t5, m - 1);
-		DivideSnow(b, t1, t2, m - 1);
-		DivideSnow(c, t3, t4, m - 1);
-	}
-	//else{
-		DrawTriangle(a, b, c);
-		//cout << a << endl;
-		//printf("%f %f %f\n", float(a), float(b), float(c));
-	//}
-}
-void MyDisplay() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_TRIANGLES);
-	DivideSnow(v[0], v[1], v[2], n);
-	glEnd();
-	glFlush();
-}
-
-void MyInit() {
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(-2.0, 2.0, -2.0, 2.0);
-	glClearColor(1.0, 1.0, 0.0, 1.0);
-	glColor3f(0.0, 0.0, 0.0);
-}
-
-int main1(int argc, char** argv) {
-	glutInit(&argc, argv);
-	n = 10;
-	scanf_s("%d", &n);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(700, 700);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("SnowFractal");
-	glutDisplayFunc(MyDisplay);
-	MyInit();
-	glutMainLoop();
-	return 0;
-}
+//#include <GL/freeglut.h>
+//#include <iostream>
+//#include <cstdio>
+//using namespace std;
+//
+//GLfloat v[3][2] = { -0.5, -0.29,
+//	0.5, -0.29,
+//	0.0, 0.575};
+//int n;
+//
+//void CalDS(GLfloat* a, GLfloat* b, GLfloat* c, GLfloat* v0, GLfloat* v1, GLfloat* v2) {
+//	for (int i = 0; i < 2; ++i) {
+//		v0[i] = (b[i] - a[i]) / 3 + a[i];
+//		v2[i] = (a[i] - b[i]) / 3 + b[i];
+//		GLfloat mid = (a[i] + b[i]) / 2;
+//		GLfloat t = mid + (mid - c[i]) / 3;
+//		v1[i] = t;
+//	}
+//}
+//
+//void DrawTriangle(GLfloat* a, GLfloat* b, GLfloat* c) {
+//	glVertex2fv(a);
+//	glVertex2fv(b);
+//	glVertex2fv(c);
+//	//getchar();
+//}
+//
+//void DivideSnow(GLfloat* a, GLfloat* b, GLfloat* c, int m) {
+//	if (m > 0) {
+//		GLfloat v0[2], v1[2], v2[2];
+//		GLfloat t0[2], t1[2], t2[2], t3[2], t4[2], t5[2];
+//		CalDS(a, b, c, v0, v1, v2);
+//		for (int i = 0; i < 2; ++i) {
+//			t0[i] = v0[i];
+//			t1[i] = v2[i];
+//		}
+//		DivideSnow(v0, v1, v2, m - 1);
+//		CalDS(b, c, a, v0, v1, v2);
+//		for (int i = 0; i < 2; ++i) {
+//			t2[i] = v0[i];
+//			t3[i] = v2[i];
+//		}
+//		DivideSnow(v0, v1, v2, m - 1);
+//		CalDS(c, a, b, v0, v1, v2);
+//		for (int i = 0; i < 2; ++i) {
+//			t4[i] = v0[i];
+//			t5[i] = v2[i];
+//		}
+//		DivideSnow(v0, v1, v2, m - 1);
+//		DivideSnow(a, t0, t5, m - 1);
+//		DivideSnow(b, t1, t2, m - 1);
+//		DivideSnow(c, t3, t4, m - 1);
+//	}
+//	//else{
+//		DrawTriangle(a, b, c);
+//		//cout << a << endl;
+//		//printf("%f %f %f\n", float(a), float(b), float(c));
+//	//}
+//}
+//void MyDisplay() {
+//	glClear(GL_COLOR_BUFFER_BIT);
+//	glBegin(GL_TRIANGLES);
+//	DivideSnow(v[0], v[1], v[2], n);
+//	glEnd();
+//	glFlush();
+//}
+//
+//void MyInit() {
+//	glMatrixMode(GL_PROJECTION);
+//	glLoadIdentity();
+//	gluOrtho2D(-2.0, 2.0, -2.0, 2.0);
+//	glClearColor(1.0, 1.0, 0.0, 1.0);
+//	glColor3f(0.0, 0.0, 0.0);
+//}
+//
+//int main1(int argc, char** argv) {
+//	glutInit(&argc, argv);
+//	n = 10;
+//	scanf_s("%d", &n);
+//	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+//	glutInitWindowSize(700, 700);
+//	glutInitWindowPosition(100, 100);
+//	glutCreateWindow("SnowFractal");
+//	glutDisplayFunc(MyDisplay);
+//	MyInit();
+//	glutMainLoop();
+//	return 0;
+//}
