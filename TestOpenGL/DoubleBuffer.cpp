0d9940fead0b933898b311476e2752e0d5abe960
liarchgit <liarchwy@163.com>
2017-09-05 09:26:42 +0800

    draw a cube

--- TestOpenGL/DoubleBuffer.cpp
+++ TestOpenGL/DoubleBuffer.cpp
@@ -18,7 +18,33 @@ struct circle {
     circle(int a, int b, int c, int d, int e, unsigned char f);
     void move(int val);
 };
+struct voc {
+    float color[4] = { 1.0, 1.0, 1.0, 1.0 };
+    float pos[3] = { 0,0,0 };
+
+    void setPos(float a, float b, float c);
+    void setColor(float a, float b, float c, float d);
+    voc(float* vpos, float* vcol);
+    voc();
+};
+struct square{
+    voc* ver[4];
+    void draw();
+};
+
+struct cube {
+    //点的顺序固定 从上往下看，先是第一层的四个点逆时针方向存储，然后是下一层四个点也是逆时针存储
+    voc vertex[8];
+    square squares[6];
+
+    //voc a, voc b, voc c, voc d, voc e, voc f, voc g, voc h
+    cube(voc* vocs);
+    cube() { setSquares(); }
+    void setSquares();
+};
+
 vector<circle>circles;
+vector<cube>cubes;
 
 void myInit();
 void myKey(unsigned char key, int x, int y);
@@ -32,7 +58,7 @@ void updateTime(int val);
 void drawSquare();
 void drawSquares();
 
-void drawCube();
+void drawCubes();
 
 void initDrawSquareCircle();
 void setOpenGL();
@@ -85,8 +111,13 @@ void drawCircles() {
 void myDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    drawSquares();
-	drawCircles();
+    if (choose == 0) {
+        drawSquares();
+        drawCircles();
+    }
+    if (choose == 1) {
+        drawCubes();
+    }
 	glutSwapBuffers();
 }
 
@@ -184,14 +215,29 @@ void drawSquares() {
 void setOpenGL() {
     printf("Choose:\n");
     printf("0: draw squares and circles\n");
+    printf("1: draw a cube\n");
     scanf_s("%d", &choose);
     switch (choose) {
         case 0:
             initDrawSquareCircle();
             break;
-        default:
-            printf("Nothin.");
+        case 1:
+            voc vocs[8];
+            vocs[0].setPos(5, 5, 5);
+            vocs[1].setPos(5, 5, -5);
+            vocs[2].setPos(-5, 5, -5);
+            vocs[3].setPos(-5, 5, 5);
+            vocs[4].setPos(5, -5, 5);
+            vocs[5].setPos(5, -5, -5);
+            vocs[6].setPos(-5, -5, -5);
+            vocs[7].setPos(-5, -5, 5);
+            cube aCube(vocs);
+            cubes.push_back(aCube);
+            initDrawSquareCircle();
             break;
+        //default:
+        //    printf("Nothin.");
+        //    break;
     }
 }
 
@@ -202,6 +248,78 @@ void initDrawSquareCircle() {
 	glutKeyboardFunc(myKey);
 	glutReshapeFunc(myReshape);
 	glutTimerFunc((1000.0) / DESIRED_FPS, updateTime, 1);
-    glutMotionFunc(addCircle);
 	glutMainLoop();
+}
+
+void drawCubes() {
+    for (int i = 0; i < cubes.size(); ++i) {
+        cube& nCube = cubes[i];
+        for (int j = 0; j < 6; ++j) {
+            nCube.squares[j].draw();
+        }
+    }
+}
+
+void square::draw() {
+    glBegin(GL_POLYGON);
+    glColor4f(1.0, 0.0, 0.0, 1.0);
+    for (int i = 0; i < 4; ++i) {
+        ((ver[i])->setColor)(1.0, 0.0, 0.0, 1.0);
+        glColor4f(((ver[i])->color)[0], ((ver[i])->color)[1], ((ver[i])->color)[2], ((ver[i])->color)[3]);
+        glVertex3f(ver[i]->pos[0], ver[i]->pos[1], ver[i]->pos[2]);
+    }
+    glEnd();
+}
+
+void cube::setSquares() {
+    for (int i = 0; i < 4; ++i) {
+        squares[0].ver[i] = &vertex[i];
+    }
+    for (int i = 4; i < 8; ++i) {
+        squares[1].ver[i] = &vertex[i];
+    }
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            squares[2 + j].ver[i] = &vertex[i + j];
+            squares[2 + j].ver[i + 2] = &vertex[i + j + 4];
+        }
+    }
+}
+
+cube::cube(voc* vocs) {
+    setSquares();
+    for (int i = 0; i < 8; ++i) {
+        vertex[i] = vocs[i];
+    }
+}
+
+voc::voc(float* vpos, float* vcol) {
+    for (int i = 0; i < 3; ++i){
+        pos[i] = vpos[i];
+    }
+    for (int i = 0; i < 4; ++i) {
+        color[i] = vcol[i];
+    }
+}
+
+void voc::setPos(float a, float b, float c) {
+    pos[0] = a;
+    pos[1] = b;
+    pos[2] = c;
+}
+
+void voc::setColor(float a, float b, float c, float d) {
+    color[0] = a;
+    color[1] = b;
+    color[2] = c;
+    color[3] = d;
+}
+
+voc::voc() {
+    for (int i = 0; i < 4; ++i) {
+        color[i] = 1.0f;
+    }
+    for (int i = 0; i < 3; ++i) {
+        pos[i] = 1.0f;
+    }
 }
